d2af1b14e63da4ba69f594c0292833236221862c
Ryan Young <ryoung@intelligentimagingsystems.com>
2018-05-04 11:42:11 -0600

    gauntletbot test

--- index.js
+++ index.js
@@ -481,20 +481,4 @@ wss.on('connection', function connection(ws) {
 
     console.log('here3');
 });
-server.listen(function listening () {
-    //
-    // If the `rejectUnauthorized` option is not `false`, the server certificate
-    // is verified against a list of well-known CAs. An 'error' event is emitted
-    // if verification fails.
-    //
-    // The certificate used in this example is self-signed so `rejectUnauthorized`
-    // is set to `false`.
-    //
-    const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
-      rejectUnauthorized: false
-    });
-  
-    ws.on('open', function open () {
-      ws.send('All glory to WebSockets!');
-    });
-  });
+server.listen(8080);
